f73b333428a21ee186197ac27ee9d9e43f39c13b
linhbuitung <linh.buitung18@gmail.com>
2024-03-14 09:47:45 +0100

    add code solution

--- s28786-pro-dl1993/s28786-pro-dl1993/Program.cs
+++ s28786-pro-dl1993/s28786-pro-dl1993/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿static int AvgCalc(int[] array)
+{
+    int avg = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        avg += array[i];
+    }
+    avg /= array.Length;
+    return avg;
+}
