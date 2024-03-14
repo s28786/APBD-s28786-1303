c3bf886c4ca8562ec02f357b1580c7a81f9325a9
linhbuitung <linh.buitung18@gmail.com>
2024-03-14 09:51:15 +0100

    implemented feature-max

--- s28786-pro-dl1993/s28786-pro-dl1993/Program.cs
+++ s28786-pro-dl1993/s28786-pro-dl1993/Program.cs
@@ -8,3 +8,17 @@
     avg /= array.Length;
     return avg;
 }
+
+static int MaxVal(int[] array)
+{
+    int max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+
+    return max;
+}
